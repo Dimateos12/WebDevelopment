fadfbd1fc86ad6a333d81517d776fc5c8eca9968
Mateusz Jablonski <mateusz.jablonski12@gmail.com>
2022-07-16 18:09:33 +0200

    Connecting Bootstrap and Jq on local machine

--- config/dbcon.php
+++ config/dbcon.php
@@ -14,7 +14,5 @@ $con = mysqli_connect($host,$username,$password,$database);
 if(!$con){
     die("Conncetion failed ". mysqli_connect_error());
 }
-else {
-    echo "Conneccted successfully";
-} 
+
 ?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <button class="btn btn-primary">Test</button>
+    <!-- Optional JavaScript -->
+    <script> src="assets/js/jquery-3.6.0.min.js"</script>
+    <script> src="assets/js/bootstrap.min.js"</script>
+   
+  </body>
+</html>
