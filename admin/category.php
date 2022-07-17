20dae0d121b5a5e80fa653db13048fce35ffb68b
Mateusz Jablonski <mateusz.jablonski12@gmail.com>
2022-07-17 21:35:39 +0200

    Wysweitlanie wszystkich kategorii

---
+++ admin/category.php
@@ -0,0 +1,80 @@
+<?php
+include("includes/header.php");
+include('../middleware/adminMiddleware.php');
+include('../config/dbcon.php');
+
+?>
+
+<?php if (isset($_SESSION['message'])) { ?>
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Hey!</strong> <?= $_SESSION['message']; ?>
+    </div>
+<?php
+    unset($_SESSION['message']);
+} ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+               <h4> Cateogries </h4>
+            </div>
+            <div class="card-body">
+                <table class="table table-border">
+                <thead>
+                    <tr>
+                        <th>
+                            ID
+                        </th>
+                        <th>
+                            Name
+                        </th>
+                        <th>
+                            Image
+                        </th>
+                        <th>
+                            Status
+                        </th>
+                        <th>
+                            Edit
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                   $categories = "categories";
+                    $query = "SELECT * FROM $categories";
+                    $category= mysqli_query($con,$query);
+                   
+                    if(mysqli_num_rows($category) > 0)
+                    {
+                        foreach($category as $item)
+                        {
+                            ?>
+                            <tr>
+                            <td><?= $item['id']; ?></td>
+                            <td><?= $item['name']; ?></td>
+                            <td>
+                                <img src="uploads/<?= $item['image']; ?>" width = 50px; height=70px; alt="<?= $item['image']; ?>">
+                            </td>
+                            <td><?= $item['status'] == '0' ? "Visible": "Hidden"; ?></td>
+                           
+                            <td>
+                                <a href="#" class="btn btn-primary"> Edit</a>
+                            </td>
+                            </tr>
+                            <?php
+                        }
+                    }
+                    ?>
+                </tbody>
+                </table>
+            </div>
+        </div>
+        </div>
+    </div>
+
+</div>
+
+<?php include("includes/footer.php"); ?>
--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -14,7 +14,7 @@
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">dashboard</i>
             </div>
-            <span class="nav-link-text ms-1">First page</span>
+            <span class="nav-link-text ms-1">Main Page  </span>
           </a>
         </li>
         <li class="nav-item">
@@ -25,6 +25,16 @@
             <span class="nav-link-text ms-1">Add Category</span>
           </a>
         </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-white " href="category.php">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">table_view</i>
+            </div>
+            <span class="nav-link-text ms-1">All Category</span>
+          </a>
+        </li>
+
       </ul>
     </div>
     <div class="sidenav-footer position-absolute w-100 bottom-0 ">
