570c974fd8f0bb86edd4602a727a2a16493b5882
Mateusz Jablonski <mateusz.jablonski12@gmail.com>
2022-07-21 12:40:17 +0200

    Change the name of the site

--- includes/header.php
+++ includes/header.php
@@ -622,7 +622,7 @@
         <nav class="navbar navbar-expand-lg custom_nav-container ">
           <a class="navbar-brand" href="index.html">
             <span>
-              Timups
+              Giacteusz Shop 
             </span>
           </a>
 
