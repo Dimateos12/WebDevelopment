5b6ac700bb5cdd7c0d5f5add1ca089e7e965a492
Mateusz Jablonski <mateusz.jablonski12@gmail.com>
2022-07-26 12:54:26 +0200

    Follow navbar

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,8 +1,11 @@
+<?php 
+  $page = substr($_SERVER['SCRIPT_NAME'],strrpos($_SERVER['SCRIPT_NAME'],"/")+1);
+?>
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
-        
+
         <span class="ms-1 font-weight-bold text-white">WATCHES SHOP </span>
       </a>
     </div>
@@ -10,15 +13,15 @@
     <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link text-white bg-gradient-primary" href="index.php">
+          <a class="nav-link text-white <?= $page == "index.php"? 'active bg-gradient-primary' : ''; ?>" href="index.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">dashboard</i>
             </div>
-            <span class="nav-link-text ms-1">Main Page  </span>
+            <span class="nav-link-text ms-1">Main Page </span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white " href="add-category.php">
+          <a class="nav-link text-white <?= $page == "add-category.php"? 'active bg-gradient-primary' : ''; ?>" href="add-category.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">table_view</i>
             </div>
@@ -27,7 +30,7 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link text-white " href="category.php">
+          <a class="nav-link text-white <?= $page == "category.php"? 'active bg-gradient-primary' : ''; ?>" href="category.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">table_view</i>
             </div>
@@ -36,7 +39,7 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link text-white " href="products.php">
+          <a class="nav-link text-white <?= $page == "products.php"? 'active bg-gradient-primary' : ''; ?>" href="products.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">table_view</i>
             </div>
@@ -45,7 +48,7 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link text-white " href="add-product.php">
+          <a class="nav-link text-white <?= $page == "add-product.php"? 'active bg-gradient-primary' : ''; ?>" href="add-product.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">table_view</i>
             </div>
