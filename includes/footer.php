8a39187c1768f7c09c7dc745562f1eaa126074db
Mateusz Jablonski <mateusz.jablonski12@gmail.com>
2022-07-27 12:16:50 +0200

    refactoring code of header and footer

--- includes/footer.php
+++ includes/footer.php
@@ -98,7 +98,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
   </script>
   <!-- custom js -->
-  <script src="js/custom.js"></script>
+  <script src="assets/js/custom.js"></script>
   <!-- Google Map -->
   <script
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&amp;callback=myMap"></script>
