3de47e67542be0f47ea05f7fd216f96bd882f7e2
Mateusz Jablonski <mateusz.jablonski12@gmail.com>
2022-07-29 01:40:58 +0200

    add message

--- customercomments.php
+++ customercomments.php
@@ -1,5 +1,12 @@
+<style>
+  .link-muted {
+    color: #aaa;
+  }
 
-<style>.link-muted { color: #aaa; } .link-muted:hover { color: #1266f1; }</style>
+  .link-muted:hover {
+    color: #1266f1;
+  }
+</style>
 
 <section style="background-color: white;">
   <div class="container my-5 py-5">
@@ -11,39 +18,39 @@
             <p class="fw-light mb-4 pb-2">Latest Comment of the user</p>
             <div class="d-flex flex-start">
 
-            <?php 
-            
-            $query = "SELECT * FROM opinions ORDER BY id DESC LIMIT 1 ";
-            $query_run = mysqli_query($con, $query);
-            $data = mysqli_fetch_array($query_run);
+              <?php
+
+              $query = "SELECT * FROM opinions ORDER BY id DESC LIMIT 1 ";
+              $query_run = mysqli_query($con, $query);
+              $data = mysqli_fetch_array($query_run);
 
 
-            if (mysqli_num_rows($query_run) > 0) {
+              if (mysqli_num_rows($query_run) > 0) {
 
 
-            foreach ($query_run as $item) {
+                foreach ($query_run as $item) {
 
 
-            
-            ?>
-            
-              <div>
-                <h6 class="fw-bold mb-1"><?= $item['user_name'] ?></h6>
-                <div class="d-flex align-items-center mb-3">
-                  <p class="mb-0">
-                  <?= $item['created_at'] ?>
-                  </p>
-                </div>
-                <p class="mb-0">
-                <?= $item['comment'] ?>
-                </p>
-              </div>
+
+              ?>
+
+                  <div>
+                    <h6 class="fw-bold mb-1"><?= $item['user_name'] ?></h6>
+                    <div class="d-flex align-items-center mb-3">
+                      <p class="mb-0">
+                        <?= $item['created_at'] ?>
+                      </p>
+                    </div>
+                    <p class="mb-0">
+                      <?= $item['comment'] ?>
+                    </p>
+                  </div>
             </div>
           </div>
-                <?php }
-            }
-                ?>
+      <?php }
+              }
+      ?>
+        </div>
       </div>
     </div>
-  </div>
 </section>
